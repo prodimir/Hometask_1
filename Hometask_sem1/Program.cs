ca4af6821326373c17a669651abea5c616935f77
prodimir <vova.prokopenko.2017@mail.ru>
2023-05-26 21:13:13 +0500

    Prokopenko_Hometask_1

---
+++ Hometask_sem1/Program.cs
@@ -0,0 +1,63 @@
+﻿/************************Task_2**************************
+Console.Write("Введите первое число: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if(y > x)
+{
+   Console.WriteLine($"{y} = max");
+}
+else
+{
+   Console.WriteLine($"{x} = max");
+}
+*/
+/************************Task_4**************************
+Console.Write("Введите первое число: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double z = Convert.ToDouble(Console.ReadLine());
+if((y > x) && (y > z) )
+{
+    Console.WriteLine($"{y} = max");
+}
+else
+{
+    if(z > x)
+        {
+            Console.WriteLine($"{z} = max");
+        }
+    else
+        {
+            Console.WriteLine($"{x} = max");
+        }   
+}*/
+
+/************************Task_6**************************
+Console.Write("Введите любое целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+    {
+        Console.Write($"{x} четное");
+    }
+else
+    {
+        Console.Write($"{x} нечетное");
+    }
+*/
+
+/************************Task_8**************************/
+Console.Write("Введите любое целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N)
+    {
+        if(i % 2 == 0)
+            {
+                Console.Write(i+" ");
+            }
+        i++;
+    }
